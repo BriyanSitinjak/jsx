8380b27c21d6d90bf8552c92d999d1dbfdcd8256
BriyanSitinjak <Imanbriyan@gmail.com>
2021-12-08 17:33:10 +0700

    styling message

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,13 +1,18 @@
 import React from "react";
 
-const Spinner = () => {
+const Spinner = (props) => {
     return (
         <div className="ui active dimmer">
             <div className="ui big text loader">
-                Loading....
+                {props.message}
             </div>
         </div>
     );
+};
+
+// defaultProps means this value will be show if the props on index.js has no value
+Spinner.defaultProps = {
+    message: 'Loading...'
 };
 
 export default Spinner;
